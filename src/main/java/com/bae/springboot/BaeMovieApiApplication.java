ab6835b4255bd58747bdf96e1a40cd09500087af
al188 <anthonylee188@yahoo.com>
2022-09-13 13:41:11 +0100

    AL- First push

---
+++ src/main/java/com/bae/springboot/BaeMovieApiApplication.java
@@ -0,0 +1,13 @@
+package com.bae.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeMovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeMovieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bae/springboot/BaeMovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bae.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeMovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
