4bdb5c83a8fc2898c5cb5bd736f85c6e51432fe1
al188 <anthonylee188@yahoo.com>
2022-09-14 13:09:41 +0100

    added a get request- getByID

--- src/main/java/com/bae/springboot/controller/MoviesController.java
+++ src/main/java/com/bae/springboot/controller/MoviesController.java
@@ -1,5 +1,9 @@
 package com.bae.springboot.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +25,9 @@ public class MoviesController {
 		return this.service.getALL();
 	}
 
+	@GetMapping("/getByID/{id}")
+	public Movies getById(@PathVariable Long id) {
+		return this.service.getByid(id);
+		
+	}
 }
