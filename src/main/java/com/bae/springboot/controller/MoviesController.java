3e3e17f8ca32f82ac435dd7b1c1d72dc1a0b048b
al188 <anthonylee188@yahoo.com>
2022-09-14 13:02:42 +0100

    AL: added get request- getAll

--- src/main/java/com/bae/springboot/controller/MoviesController.java
+++ src/main/java/com/bae/springboot/controller/MoviesController.java
@@ -7,4 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/Movies") // adds a prefix to the request URL
 public class MoviesController {
 
+	private MoviesServices service;
+	
+	public MoviesController(MoviesServices service) {
+		super();
+		this.service = service;
+	}
+
+	//get requests (Read - getall)
+	
+	@GetMapping("getAll")
+	public List <Movies> getAll(){
+		return this.service.getALL();
+	}
+
 }
