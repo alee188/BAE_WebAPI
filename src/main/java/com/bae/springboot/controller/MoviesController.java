5d0aeeb47d49c2888c96d77a3f0e53543c67414a
al188 <anthonylee188@yahoo.com>
2022-09-14 13:27:46 +0100

    AL: added post mapping- create

--- src/main/java/com/bae/springboot/controller/MoviesController.java
+++ src/main/java/com/bae/springboot/controller/MoviesController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bae.springboot.entities.Movies;
+
 @RestController
 @RequestMapping("/Movies") // adds a prefix to the request URL
 public class MoviesController {
