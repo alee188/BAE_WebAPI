eb0b50a35b3dc20a2eb702ad328cbcfef8b1ed9a
al188 <anthonylee188@yahoo.com>
2022-09-14 12:51:38 +0100

    AL: added RestController and RequestMapping

--- src/main/java/com/bae/springboot/controller/MoviesController.java
+++ src/main/java/com/bae/springboot/controller/MoviesController.java
@@ -1,5 +1,10 @@
 package com.bae.springboot.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Movies") // adds a prefix to the request URL
 public class MoviesController {
 
 }
