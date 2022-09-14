b47b0fa6edb57a02a7cb107fcabad17eb1234239
al188 <anthonylee188@yahoo.com>
2022-09-14 11:56:57 +0100

    AL: added getters and setters

--- src/main/java/com/bae/springboot/entities/Movies.java
+++ src/main/java/com/bae/springboot/entities/Movies.java
@@ -28,12 +28,53 @@ public class Movies {
 	// default constructor
 	public Movies () {}
 
+	// getters and setters
 	public Movies(long id, String title, String genre, int releaseYear, float userRating) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.genre = genre;
 		this.releaseYear = releaseYear;
+		this.userRating = userRating;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public int getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(int releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+	public float getUserRating() {
+		return userRating;
+	}
+
+	public void setUserRating(float userRating) {
 		this.userRating = userRating;
 	}
 	
